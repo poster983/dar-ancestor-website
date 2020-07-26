059d5664b54af62ce02331e897fe46cb95d15cfe
Joseph Hassell <josephh2018@gmail.com>
2020-07-25 20:10:51 -0500

    Removed attribution

--- src/js/elements/ancestor-bottom-bar.js
+++ src/js/elements/ancestor-bottom-bar.js
@@ -109,7 +109,7 @@ import * as common from "../common";
       return html`
 
       <div id="bar" class="bar">
-            <div class="text"> <a href="https://github.com/poster983" id="attribution" target="_blank">Created with &#x2764; by Joey</a></div>
+            <!--<div class="text"> <a href="github.josephhassell.dev" id="attribution" target="_blank">Created with &#x2764; by Joey</a></div>-->
             <div id="updated" class="text">Last Updated: ${this.lastUpdated}</div>
             <!--Webmaster-->
             ${!(this.webmasterEmail == null || this.webmasterEmail == undefined || this.webmasterEmail == "") ? html`
