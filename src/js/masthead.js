e8641fe53c1c96e5134f310ef6e386aadf9f540e
Joseph Hassell <josephh2018@gmail.com>
2020-12-01 15:06:44 -0600

    Added Dropdown to navbar.

--- src/js/masthead.js
+++ src/js/masthead.js
@@ -1,15 +1,18 @@
-import {default as config} from "../../ancestor-config.json";
+import {
+    default as config
+} from "../../ancestor-config.json";
 
+let mastheadTitle = document.getElementById("masthead-title");
+
+let dropdownContainer = document.getElementById("morph-dropdown");
+
+let drawerLinks = document.getElementById("drawer-links");
+
+let drawerTitle = document.getElementById("drawer-title");
+let drawerSubtitle = document.getElementById("drawer-subtitle");
 
 export function buildMasthead() {
-    let mastheadTitle = document.getElementById("masthead-title");
 
-    let dropdownContainer = document.getElementById("morph-dropdown");
-    
-    let drawerLinks = document.getElementById("drawer-links");
-
-    let drawerTitle = document.getElementById("drawer-title");
-    let drawerSubtitle = document.getElementById("drawer-subtitle");
     // set titles 
     drawerSubtitle.innerHTML = config.title;
     drawerTitle.innerHTML = config.organizationName;
@@ -17,36 +20,17 @@ export function buildMasthead() {
 
 
     // loop through config for links
-    for(let x = config.mastheadLinks.length-1; x >= 0; x--) {
+    for (let x = config.mastheadLinks.length - 1; x >= 0; x--) {
         let link = config.mastheadLinks[x];
+
         /* FIRST BUILD MASTHEAD  */
-        //make button
-        let button = document.createElement("mwc-button");
-
-        //config button
-        //button.setAttribute("slot", "actionItems");
-        //button.setAttribute("outlined", true);
-        button.style.setProperty("--mdc-theme-primary", "rgb(255,255,255)");
-        button.classList.add("masthead-links");
-
-        if(link.newWindow) {
-            button.setAttribute('trailingIcon', true);
-            button.setAttribute("icon", "open_in_new")
+        if (link.dropdown != null) {
+            buildDropdown(link);
+        } else {
+            buildButton(link);
         }
+        
 
-        //set text
-        button.setAttribute("label", link.name);
-        //set click action
-        button.addEventListener("click", () => {
-            if(link.newWindow) {
-                window.open(link.href, '_blank');
-            } else {
-                window.location.href = link.href;
-            }
-            
-        }, {passive: true})
-
-        dropdownContainer.prepend(button);
 
         /* THEN BUILD THE SIDE NAV */
 
@@ -57,7 +41,7 @@ export function buildMasthead() {
         listItem.innerHTML = link.name;
 
         //add new window icon
-        if(link.newWindow) {
+        if (link.newWindow) {
             listItem.setAttribute("hasMeta", true);
             let icon = document.createElement("mwc-icon");
             icon.setAttribute("slot", "meta");
@@ -67,22 +51,110 @@ export function buildMasthead() {
 
         //set click action
         listItem.addEventListener("click", () => {
-            if(link.newWindow) {
+            if (link.newWindow) {
                 window.open(link.href, '_blank');
             } else {
                 window.location.href = link.href;
             }
-            
-        }, {passive: true})
+
+        }, {
+            passive: true
+        })
 
         drawerLinks.prepend(listItem);
 
     }
 
-    
+
 }
 
 
+function buildButton(link) {
+    //make button
+    let button = document.createElement("mwc-button");
+
+    //config button
+    //button.setAttribute("slot", "actionItems");
+    //button.setAttribute("outlined", true);
+    button.style.setProperty("--mdc-theme-primary", "rgb(255,255,255)");
+    button.classList.add("masthead-links");
+
+    if (link.newWindow) {
+        button.setAttribute('trailingIcon', true);
+        button.setAttribute("icon", "open_in_new")
+    }
+
+    //set text
+    button.setAttribute("label", link.name);
+    //set click action
+    button.addEventListener("click", () => {
+        if (link.newWindow) {
+            window.open(link.href, '_blank');
+        } else {
+            window.location.href = link.href;
+        }
+
+    }, {
+        passive: true
+    })
+
+    dropdownContainer.prepend(button);
+}
+
+/**
+ * 
+ * @param {Object} item 
+ * @param {Object[]} item.dropdown
+ * @param {String} item.name
+ */
 function buildDropdown(item) {
+    
+    let container = document.createElement("morph-dropdown-item");
+    let button = document.createElement("mwc-button");
+    let list = document.createElement("mwc-list");
+
+    button.setAttribute("slot", "button");
+    list.setAttribute("slot", "dropdown");
+    
+
+    button.style.setProperty("--mdc-theme-primary", "rgb(255,255,255)");
+    button.classList.add("masthead-links");
+    list.classList.add("masthead-dropdown-links");
+
+    button.setAttribute("label", item.name);
+
+
+
+    item.dropdown.forEach(element => {
+        if(element.href == null) {
+            console.error("Dropdown children must have an href property")
+            throw new TypeError("Dropdown children must have an href property");
+        }
+        let link = document.createElement("a");
+        link.href = element.href;
+        
+        let listItem = document.createElement("mwc-list-item");
+        listItem.innerHTML = element.name;
+        
+        if(element.newWindow) {
+            link.target = "_blank";
+            listItem.setAttribute("hasMeta", true);
+            let icon = document.createElement("mwc-icon");
+            icon.setAttribute("slot", "meta");
+            icon.innerHTML = "open_in_new";
+            listItem.appendChild(icon);
+        }
+
+        link.append(listItem);
+
+
+        list.append(link);
+        
+    });
+
+    container.append(button);
+    container.append(list);
+
+    dropdownContainer.prepend(container);
 
 }
